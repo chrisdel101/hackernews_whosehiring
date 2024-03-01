f83b8523bedaefbd00416934c38a85663490da64
Chris Del <drumgod101@gmail.com>
2024-03-01 15:45:35 -0600

    add toggle buttons; try adding main component

---
+++ src/components/ToggleButtonGroup.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
+
+interface IProps {
+    filter?: string | null 
+    handleFilter?: (event: React.MouseEvent<HTMLElement>, newFilter: string | null) => void
+}
+export default function ToggleButtons({filter, handleFilter}: IProps) {
+
+  return (
+    <ToggleButtonGroup
+      value={filter}
+      exclusive
+      onChange={handleFilter}
+      aria-label="job order filter"
+    >
+      <ToggleButton value="newest" aria-label="date newest">
+       Date Newest 
+      </ToggleButton>
+      <ToggleButton value="oldest" aria-label="date oldest">
+       Date Oldest 
+      </ToggleButton>
+      <ToggleButton value="country" aria-label="country">
+      Country
+      </ToggleButton>
+      <ToggleButton value="reset" aria-label="resest" disabled>
+       X
+      </ToggleButton>
+    </ToggleButtonGroup>
+  );
+}
